a3b96f09405e1ce5fdc913efd5a2458152b7293f
anthox-ico <42335066+anthox-ico@users.noreply.github.com>
2018-11-14 21:20:07 +0100

    Add files via upload

---
+++ _DEMO.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _DEMO : MonoBehaviour {
+
+    public GameObject player;
+    public Camera _camera;
+    public _map map;
+	// Use this for initialization
+	void Start () {
+
+        map.generate_map();
+        _character character = player.GetComponent<_character>();
+        character.set_current_cell(map.get_start_cell(player));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _character chara = player.GetComponent<_character>();
+        if (chara.return_path_ended() == true)
+        {
+            map.clear_cells(chara.return_walkable_cells());
+            show_movement_possibilities();
+            chara.set_path_ended(false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject hit = map.case_touchee(_camera);
+            hit.GetComponent<_cell>().change_color(Color.red);
+            hit.GetComponent<_cell>().set_up_is_obstacle(true);
+        }
+		
+	}
+    public void show_movement_possibilities()
+    {
+        _character character = player.GetComponent<_character>();
+        character.set_walkable_cells(map.get_walkable_cells(character.return_current_cell(), character.max_movement));
+        map.change_cells_colors(character.return_walkable_cells(), Color.blue);
+    }
+    void waiting_click_to_move()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1) && player.GetComponent<_character>().return_is_walking() == false)
+        {
+            _character character = player.GetComponent<_character>();
+            if (character.return_walkable_cells() != null)
+                map.clear_cells(character.return_walkable_cells());
+            show_movement_possibilities();
+            GameObject hit = map.case_touchee(_camera);
+            bool is_hit_ok = map.check_hit_cell(character.return_walkable_cells(), hit);
+            if (is_hit_ok == true)
+            {
+                character.set_move(true);
+                character.set_path(map.get_path(character.return_current_cell(), hit));
+            }
+            if (is_hit_ok == false)
+            {
+                character.set_move(false);
+            }
+
+        }
+    }
+}
---
+++ _character.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _character : MonoBehaviour {
+
+    public int max_movement = 6;
+    public int speed = 2;
+
+    bool is_walking = false;
+    bool path_ended = false;
+    GameObject current_cell = null;
+    GameObject[] walkable_cells = null;   
+    GameObject[] path = null;
+
+    // Use this for initialization
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (is_walking == true)
+        {
+            move();
+        }
+    }
+
+    /* cells */
+    public void set_current_cell(GameObject c)
+    {
+        current_cell = c;
+    }
+    public GameObject return_current_cell()
+    {
+        return current_cell;
+    }
+    // movement
+    public bool return_is_walking()
+    {
+        return is_walking;
+    }
+    public void set_walkable_cells(GameObject[] c)
+    {
+        walkable_cells = c;
+    }
+    public void set_path(GameObject[] p)
+    {
+        path = p;
+    }
+    public GameObject[] return_path()
+    {
+        return path;
+    }
+    public GameObject[] return_walkable_cells()
+    {
+        return walkable_cells;
+    }
+    public void set_move(bool b)
+    {
+        is_walking = b;
+    }
+    public void move()
+    {
+        int count = 0;
+        while (path[count] == null && count < path.Length - 1)
+        {
+            count++;
+        }
+        if (Vector3.Distance(gameObject.transform.position, path[count].transform.position) >= 0.2f)
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, path[count].transform.position, gameObject.GetComponent<_character>().speed * Time.deltaTime);
+        }
+        if (Vector3.Distance(gameObject.transform.position, path[count].transform.position) <= 0.2f)
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, path[count].transform.position, gameObject.GetComponent<_character>().speed * Time.deltaTime);
+            current_cell = path[count];
+            path[count] = null;
+            if (count == path.Length - 1)
+            {
+                Debug.Log("char arv case finale");
+                path_ended = true;
+                path = null;
+                is_walking = false;
+            }
+        }
+    }
+    public bool return_path_ended()
+    {
+        return path_ended;
+    }
+    public void set_path_ended(bool b)
+    {
+        path_ended = b;
+    }
+}
